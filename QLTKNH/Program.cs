3ae0220cc1b38bbf1f7921a60e9c7450c444a3d6
tienmeomeo <tiennn107@gmail.com>
2024-04-01 16:31:32 +0700

    login

--- QLTKNH/Program.cs
+++ QLTKNH/Program.cs
@@ -16,7 +16,7 @@ namespace QLTKNH
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmDangNhap());
         }
     }
 }
